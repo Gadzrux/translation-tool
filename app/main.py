de0b624937351962c4bfbe099f4a3d254591d304
Krishnava <krishnavaghosh23@gmail.com>
2025-02-11 08:08:56 +0530

    index.html done

--- app/main.py
+++ app/main.py
@@ -1,17 +1,13 @@
-from fastapi import FastAPI, Request, HTTPException, Depends, BackgroundTasks
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.middleware.cors import CORSMiddleware
-
 from fastapi.templating import Jinja2Templates
 
-#setup templates
-templates = Jinja2Templates(directory="./templates")
-
 app = FastAPI()
+templates = Jinja2Templates("./templates")
 
-@app.get('/index',response_class=HTMLResponse)
-def index(request: Request):
+@app.get("/", response_class=HTMLResponse)
+def send_template(request: Request):
     return templates.TemplateResponse(
-        name="index.html",
-        context={"request": request}
+        request,
+        name="index.html"
         )
